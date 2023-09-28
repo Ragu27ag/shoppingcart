0f1684798b0d48d67b0975e183228fb861515c39
ragunath <raguyuvi27@gmail.com>
2023-09-28 12:33:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,58 @@
+import "./App.css";
 
-import './App.css';
+import Cart from "./Cart/Cart";
+import HeaderCompnent from "./Cart/HeaderCompnent";
+import FooterComponent from "./Cart/FooterComponent";
 
-import Cart from './Cart/Cart';
-import Comp1 from './Cart/Comp1';
-import Comp2 from './Cart/Comp2';
-
-import './Cart/Cart.css'
-import { useState } from 'react';
-
-
+import "./Cart/Cart.css";
+import { useState } from "react";
 
 const data = [
-  { product: 'Fancy product', price: '$40.00-$80.00' },
-  { product: 'Special Item', price: '$20.00 $18.00' },
-  { product: 'Sale Item', price: '$50.00 $25.00' },
-  { product: 'Popular Item', price: '$40.00' },
-  { product: 'Sale Item', price: '$50.00 $25.00' },
-  { product: 'Fancy product', price: '$120.00-$280.00' },
-  { product: 'Special Item', price: '$20.00 $18.00' },
-  { product: 'Popular Item', price: '$40.00' }
-]
-
-
-
+  { id: 1, product: "Fancy product", price: "$40.00-$80.00" },
+  { id: 2, product: "Special Item", price: "$20.00 $18.00" },
+  { id: 3, product: "Sale Item", price: "$50.00 $25.00" },
+  { id: 4, product: "Popular Item", price: "$40.00" },
+  { id: 5, product: "Sale Item", price: "$50.00 $25.00" },
+  { id: 6, product: "Fancy product", price: "$120.00-$280.00" },
+  { id: 7, product: "Special Item", price: "$20.00 $18.00" },
+  { id: 8, product: "Popular Item", price: "$40.00" },
+];
 
 function App() {
   const [addp, setAdd] = useState([]);
 
-  console.log(addp)
+  console.log(addp);
 
-  const addPro = (prod) => {
-    setAdd([...addp, prod]);
-  }
+  const addPro = (id) => {
+    const cartProduct = data.filter((val) => val.id === id);
+    console.log(cartProduct);
+    setAdd([...addp, ...cartProduct]);
+  };
 
   const removePro = (p) => {
-    setAdd(addp.filter((pr) => pr !== p));
-  }
-
- 
+    setAdd(addp.filter((pr) => pr.id !== p));
+  };
 
   return (
-
-
     <div className="App">
-      <Comp1 addp={addp} />
-      <div className='cartdiv'>
+      <HeaderCompnent addp={addp} />
+      <div className="cartdiv">
         {data.map((pro) => {
           return (
-
             <Cart
+              id={pro.id}
               product={pro.product}
               price={pro.price}
-              buttn={addp.find((prr) => prr === pro.product)}
+              buttn={addp.find((prr) => prr.id === pro.id)}
               addPro={addPro}
               removePro={removePro}
-              key={pro.product}
+              key={pro.id}
             />
-
-          )
+          );
         })}
       </div>
-      <Comp2/>
+      <FooterComponent />
     </div>
-
-
-
-
   );
 }
 
---
+++ src/Cart/FooterComponent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Cart.css";
+
+function FooterComponent() {
+  return (
+    <>
+      <div
+        className="containerxl"
+        style={{
+          height: "100px",
+          backgroundColor: "#212529",
+        }}
+      >
+        <p style={{ margin: "30px" }} className="conp">
+          Copyright © Your Website 2023
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FooterComponent;
